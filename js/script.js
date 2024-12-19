7d8bdec2364a579d98d170ba545b273092ead604
Sachin Chanduveetil <sachisavera@gmail.com>
2024-12-19 11:28:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// .closest() Polyfill for browsers that supports document.querySelectorAll() - IE9 again...
-if (window.Element && !Element.prototype.closest) {
-    Element.prototype.closest =
-    function(s) {
-        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-            i,
-            el = this;
-        do {
-            i = matches.length;
-            while (--i >= 0 && matches.item(i) !== el) {};
-        } while ((i < 0) && (el = el.parentElement));
-        return el;
-    };
-}
 
 // for active menu highlighting
 
